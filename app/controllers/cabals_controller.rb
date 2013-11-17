f5532422f0774c7ff4cb8acb8c741f7f39ca1fcb
ychen022 <ychen91@mit.edu>
2013-11-17 10:22:12 -0500

    Added invite to cabal. Using a form atm

--- app/controllers/cabals_controller.rb
+++ app/controllers/cabals_controller.rb
@@ -1,5 +1,6 @@
 class CabalsController < ApplicationController
 	before_action :authenticate_user!
+	before_action :set_cabal, only: [:show, :add_member]
 	
 	def index
 		@cabals=[]
@@ -18,9 +19,23 @@ class CabalsController < ApplicationController
 	end
 	
 	def show
-		@cabal=Cabal.find_by_id(params[:id])
+	end
+	
+	# Add a member to the cabal. Will change to ajax later.
+	def add_member
+		result=@cabal.add_member(params[:username])
+		if result==0
+			flash.now[:alert] = "User does not exist"
+		elsif result==-1
+			flash.now[:alert] = "User already in cabal"
+		else
+			flash.now[:notice] = "Successfully added user to cabal"
+		end
+		render :show 
 	end
 	
 	private
-		
+		def set_cabal
+			@cabal=Cabal.find_by_id(params[:id])
+		end
 end
--- app/models/cabal.rb
+++ app/models/cabal.rb
@@ -1,20 +1,44 @@
 class Cabal < ActiveRecord::Base
 	has_and_belongs_to_many :users, class_name: 'User', join_table: 'cabal_users'
 	
+	# Creates a new cabal from the parameters and associates cabals and users
+	# TODO use errors properly
 	def self.create_new_from_params(user,params)
 		name=params[:name]
 		date=params[:date]
 		membersRaw=params[:members]
-		members=membersRaw.split(/,/)
+		members=membersRaw.split(/\s*,\s*/)
 		cabal=user.cabals.create(cabal_name:name, activity_date:date)
 		members.each do |m|
 			iu=User.find_by_username(m)
 			if !iu.nil?
-				#iu.cabals<<cabal
-				cabal.users<<iu
+				if cabal.users.include?(iu)
+					cabal.errors.add(:username, "User already in cabal")
+				else
+					cabal.users<<iu
+				end
+			else
+				cabal.errors.add(:username, "User not found")
 			end
 		end
 		return cabal
+	end
+	
+	# Take a username as an input
+	# Returns 0 for nonexistant user, 1 for success, -1 for user already in cabal
+	def add_member(user)
+		u=User.find_by_username(user)
+		if u.nil?
+			self.errors.add(:username, "User does not exist")
+			return 0
+		else
+			if !self.users.include?(u)
+				self.users<<u
+				return 1
+			else
+				return -1
+			end
+		end
 	end
 	
 	def name
