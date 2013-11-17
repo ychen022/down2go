2b39b616c860ef4978639a2ccba2c2e10fc21347
ychen022 <ychen91@mit.edu>
2013-11-17 10:45:00 -0500

    Signup fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
+         :recoverable, :rememberable, :trackable, :validatable
+	validates_presence_of :username
+  validates_uniqueness_of :username
 end
