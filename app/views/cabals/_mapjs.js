355c242d604027182b7316b14eeacd2e15324c82
ychen022 <ychen91@mit.edu>
2013-12-08 03:04:23 -0500

    Basic direction checking (only displaying travel time) added

--- app/views/cabals/_mapjs.js
+++ app/views/cabals/_mapjs.js
@@ -37,6 +37,7 @@ var initialize=function() {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    directionsService = new google.maps.DirectionsService();
     <% @pinpoints.each do |p| %>
         var place_id = '<%= p.id %>';
     var place_name = '<%= p.place %>';
@@ -63,7 +64,109 @@ var add_pin=function(id, place, time) {
     console.log("Pin added");
 }
 
+var get_direction=function(start, end, car){
+  if (car){
+    var request = {
+      origin:start.place,
+      destination:end.place,
+      travelMode:google.maps.TravelMode.DRIVING
+    };
+    directionsService.route(request, function(result, status) {
+      console.log("drive: "+status);
+      if (status == google.maps.DirectionsStatus.OK) {
+        //directionsDisplay.setDirections(result);
+        var dResult = result;
+        var leave_at = time_to_utc(end.time)-result.routes[0].legs[0].duration.value;
+        update_agenda_with_direction(start, end, dResult, leave_at);
+      }
+    });
+  }else{
+    var endtime = new Date(time_to_utc(end.time)*1000);
+    var transit_request = {
+      origin:start.place,
+      destination:end.place,
+      travelMode:google.maps.TravelMode.TRANSIT,
+      transitOptions: {arrivalTime:endtime}
+    };
+    var walk_request = {
+      origin:start.place,
+      destination:end.place,
+      travelMode:google.maps.TravelMode.WALKING
+    };
+    directionsService.route(transit_request, function(result, status) {
+      console.log("transit: "+status);
+      var tResult;
+      if (status == google.maps.DirectionsStatus.OK) {
+        //directionsDisplay.setDirections(result);
+        tResult = result;
+      }
+      directionsService.route(walk_request, function(result, status) {
+        console.log("walk: "+status);
+        if (status == google.maps.DirectionsStatus.OK) {
+          //directionsDisplay.setDirections(result);
+          var dResult;
+          var leave_at;
+          if (!tResult || (time_to_utc(end.time)-result.routes[0].legs[0].duration.value)>tResult.routes[0].legs[0].departure_time.value){
+            dResult = result;
+            leave_at = time_to_utc(end.time)-result.routes[0].legs[0].duration.value;
+            update_agenda_with_direction(start, end, dResult, leave_at);
+          }else{
+            dResult = tResult;
+            leave_at = tResult.routes[0].legs[0].departure_time.value;
+            update_agenda_with_direction(start, end, dResult, leave_at);
+          }
+        }
+      });
+    });
+    
+  }
+  
+}
 
+var update_agenda_with_direction = function(start, end, dResult, leave_at){
+  console.log("Updating agenda: "+start.id);
+  var vDiv = $('#'+start.id);
+  if (vDiv.children('div.route_info')!=0){
+    $('div.route_info','#'+start.id).remove();
+  }
+  if (leave_at<time_to_utc(start.time)){
+    // Display error message
+    vDiv.append('<div class="route_info">\
+    You will not make it to the next agenda item in time. <br />\
+    The travel time required is '+dResult.routes[0].legs[0].duration.text+'.\
+    </div>');
+    console.log("agenda error appended");
+  }else{
+    // Display ideal departure time
+    var nd = new Date(leave_at*1000);
+    vDiv.append('<div class="route_info">\
+    You need to leave for the next agenda item at '+nd.toUTCString()+'. <br />\
+    The travel time required is '+dResult.routes[0].legs[0].duration.text+'.\
+    </div>');
+    console.log('<div class="route_info">\
+    You need to leave for the next agenda item at '+nd.toUTCString()+'. <br />\
+    The travel time required is '+dResult.routes[0].legs[0].duration.text+'.\
+    </div>');
+    console.log("agenda notice appended");
+  }
+}
+
+// Calculate the difference in seconds of two time strings in HH:MM AM/PM
+var time_difference = function(timea, timeb){
+  return time_to_utc(timeb)-time_to_utc(timea);
+}
+
+var time_to_utc = function(tString){
+  var hr = parseInt(tString.substring(0,2));
+  if ((tString.substring(6,8)==="PM") && (tString.substring(0,2)!="12")){
+    hr+=12;
+  }else if((tString.substring(6,8)==="AM") && (tString.substring(0,2)==="12")){
+    hr=0;
+  }
+  var dateStr = $('.cabal-date-hidden').val();
+  var dayTime = new Date(dateStr);
+  return (hr*60+parseInt(tString.substring(3,5)))*60+dayTime.getTime()/1000;
+}
 
 $(function(){
     initialize();
@@ -81,5 +184,14 @@ $(function(){
             }
         });
     });
+    
+    $('#check_agenda').click(function(){
+      var car=$('#has_car').prop('checked');
+      var aInfo = agenda_info.all();
+      for (var i=0;i<aInfo.length-1;i++){
+        get_direction(aInfo[i], aInfo[i+1], car);
+      }
+    });
+      
 });
 </script>
